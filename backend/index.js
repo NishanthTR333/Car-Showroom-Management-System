440d0ce214fc0048a0a658c345695ba5f42adc06
NishanthTR333 <159543691+NishanthTR333@users.noreply.github.com>
2025-10-31 10:03:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config(); // Make sure this is at the top
+require('dotenv').config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// --- API Routes ---
-// Any request to /api/auth will be handled by your auth.js file
+// API Routes
+
 app.use("/api/auth", require("./routes/auth"));
 
-// Any request to /api/cars will be handled by your cars.js file
+
 app.use("/api/cars", require("./routes/cars"));
 
-// Any request to /api/orders will be handled by your orders.js file
+
 app.use("/api/orders", require("./routes/orders"));
-// --- End Routes ---
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
